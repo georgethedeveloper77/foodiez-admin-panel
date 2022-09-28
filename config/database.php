47f3986209a80ef3cf6739e770a71c640bddde5e
georgethedeveloper77 <georgekarani77@gmail.com>
2022-09-28 21:17:51 +0300

    env

--- config/database.php
+++ config/database.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL = parse_url('postgres://wrwjucammjxpzu:62dde95762b0f5804ea8f4ddddd04a2bfaca79f098b449052bbede129bff1a8e@ec2-54-152-28-9.compute-1.amazonaws.com:5432/dar464275ikc08');
-
 return [
 
     /*
@@ -17,7 +15,7 @@ return [
     |
      */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,15 +65,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $DATABASE_URL["ec2-54-152-28-9.compute-1.amazonaws.com"],
-            'port' => $DATABASE_URL["5432"],
-            'database' => ltrim($DATABASE_URL["postgres://wrwjucammjxpzu:62dde95762b0f5804ea8f4ddddd04a2bfaca79f098b449052bbede129bff1a8e@ec2-54-152-28-9.compute-1.amazonaws.com:5432/dar464275ikc08"], "/"),
-            'username' => $DATABASE_URL["dar464275ikc08"],
-            'password' => $DATABASE_URL["62dde95762b0f5804ea8f4ddddd04a2bfaca79f098b449052bbede129bff1a8e"],
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
